6215f51ca0020652cbc8a029935f0c79b7590cef
Shaikh Noor Abbas <shaikhnoor1512@gmail.com>
2025-11-29 17:22:09 +0530

    fixed express.Router() issue

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -5,16 +5,27 @@ const routers = express.Router();
 
 // user Route Register
 // @api dsc -> register
-//@api method -> post
+//@api method -> POST
 //@api endpoint -> /api/user/register
 
 routers.post("/register", register);
 
 // user Route Login
 // @api dsc -> login
-//@api method -> post
+//@api method -> POST
 //@api endpoint -> /api/user/login
 
 routers.post("/login", login);
+
+// user Route Get Test
+// @api dsc -> getTest
+// @api method -> GET
+//@api endpoint -> /api/user/getTest
+
+routers.get("/getTest", (req, res) => {
+  res.json({
+    message: "This is a Sample Test for Get Request",
+  });
+});
 
 export default routers;
